2958cad4c6fdb3a2b9859e0edfde0f48952a0725
Mark33Mark <mark@watsonised.me>
2026-06-17 22:47:40 +1000

    [BUG] fixed issue with download filename

--- src/utilities/FileManagement.js
+++ src/utilities/FileManagement.js
@@ -5,12 +5,13 @@ export const getLocalFileName = (baseName, extension) => {
         day: '2-digit',
         hour: '2-digit',
         minute: '2-digit',
-        second: '2-digit',
         hour12: false,
     }).format(new Date());
 
     // Replace file system unsafe characters (like '/' and ':') and spaces with hyphens
-    const safeDate = localDate.replace(/[/:\s]/g, '-');
+    const safeDate = localDate.replace(/[/:,]/g, '').replace(/\s/g, '-');
+
+        console.log('localDate = ', safeDate)
 
     return `${safeDate}_${baseName}.${extension}`;
 };
